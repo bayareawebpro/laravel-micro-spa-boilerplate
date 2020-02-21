139735adb24ef28e241f6141a121a1ee74fdd7cd
Dan Alvidrez <dan@bayareawebpro.com>
2020-02-21 00:14:40 -0800

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,21 @@
 <?php
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\{Route};
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
 */
+
+Route::any('errors/500',fn()=>abort(500, 'Server Error'));
+Route::any('errors/401',fn()=>abort(401, 'UnAuthenticated'));
+Route::any('errors/403',fn()=>abort(403, 'UnAuthorized / Forbidden'));
+Route::any('errors/404',fn()=>abort(404, 'Not Found'));
+Route::any('errors/419',fn()=>abort(419, 'Session Expired'));
+Route::any('errors/422',fn()=> ValidationException::withMessages([
+    'field' => ['message1','message2']
+]));
 
 Route::group(['middleware' => 'auth:airlock'], function(){
     Route::resource('users','UserController');
