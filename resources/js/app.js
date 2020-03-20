d1f8b930053864ffff5b342e0b2a3f351dc9c430
Dan Alvidrez <dan@bayareawebpro.com>
2020-03-19 19:16:45 -0700

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ import Application from "./application"
 import AutoLoader from "./utilities/AutoLoader"
 import ErrorHandler from "./Utilities/ErrorHandler"
 import {AppServiceProvider, Repository} from "laravel-micro.js"
-import Route from "./Services/Router/VueRoute"
 /**
  * Boot Application
  */
@@ -18,10 +17,11 @@ App.register(AppServiceProvider)
 App.setInstance('AutoLoader', AutoLoader)
 App.loadProviders()
 App.bootProviders()
-App.make('VueRoot').$nextTick(()=>{
-    const config = App.make('Config')
-    if(['production'].includes(config.get('environment'))){
-        App.make('Worker').then((worker) => worker.install())
-    }
-    console.log('Loaded')
-})
+App.make('VueRoot')
+// App.make('VueRoot').$nextTick(()=>{
+//     const config = App.make('Config')
+//     if(['production'].includes(config.get('environment'))){
+//         App.make('Worker').then((worker) => worker.install())
+//     }
+//     console.log('Loaded')
+// })
