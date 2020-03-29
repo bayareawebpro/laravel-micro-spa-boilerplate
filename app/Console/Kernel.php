1c8102eaefcb80315f87fd639f3f8119cb75df28
Dan Alvidrez <dan@bayareawebpro.com>
2020-03-28 22:20:07 -0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,15 +19,17 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule
-             ->command('migrate:fresh', ['--force' => true])
-             ->then(fn() => $this->call('db:seed'))
-             ->everyFifteenMinutes();
+        $schedule
+            ->command('migrate:fresh', ['--force' => true])
+            ->everyFifteenMinutes();
+        $schedule
+            ->command('db:seed', ['--force' => true])
+            ->everyFifteenMinutes();
     }
 
     /**
@@ -37,7 +39,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
