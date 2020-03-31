cde6fd703ef928a72ce4e12a92b85496f1f10f5e
Dan Alvidrez <dan@bayareawebpro.com>
2020-03-31 07:06:09 -0700

    wip

--- resources/js/Services/Router/ScrollBehavior.js
+++ resources/js/Services/Router/ScrollBehavior.js
@@ -3,7 +3,7 @@ import VueScrollTo from "vue-scrollto"
 export default (to, from, last) => {
     return new Promise((resolve) => {
         VueScrollTo.scrollTo("#header", 300, {
-            offset: last ? last.y : 0,
+            offset: 0,
             container: 'body',
             easing: "ease",
             onDone: resolve,
