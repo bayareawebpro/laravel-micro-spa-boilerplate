d64b739ef710a1d8f7801cbcb08978af3467e03c
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-13 18:37:29 -0700

    Remove $options usage from viewport mixin

--- resources/js/Components/Mixins/Viewport.js
+++ resources/js/Components/Mixins/Viewport.js
@@ -1,24 +1,32 @@
 export default {
     data(){
         return {
-            viewport: {}
+            viewport: {
+                width: window.innerWidth,
+                height: window.innerHeight,
+                isSmall: window.innerWidth <= 640,
+                isMedium: window.innerWidth <= 768,
+                isLarge: window.innerWidth >= 1024,
+                isXL: window.innerWidth >= 1280,
+            }
         }
     },
-    beforeCreate() {
-        this.$options.syncViewPort = ()=>(this.viewport = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-            isSmall: window.innerWidth <= 640,
-            isMedium: window.innerWidth <= 768,
-            isLarge: window.innerWidth >= 1024,
-            isXL: window.innerWidth >= 1280,
-        })
+    methods:{
+        syncViewPort(){
+            this.viewport = {
+                width: window.innerWidth,
+                height: window.innerHeight,
+                isSmall: window.innerWidth <= 640,
+                isMedium: window.innerWidth <= 768,
+                isLarge: window.innerWidth >= 1024,
+                isXL: window.innerWidth >= 1280,
+            }
+        }
     },
     created() {
-        this.$options.syncViewPort()
-        window.addEventListener('resize', this.$options.syncViewPort)
+        window.addEventListener('resize', this.syncViewPort)
     },
     beforeDestroy(){
-        window.removeEventListener('resize', this.$options.syncViewPort)
+        window.removeEventListener('resize', this.syncViewPort)
     }
 }
