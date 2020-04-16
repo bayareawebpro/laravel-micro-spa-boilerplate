1d1266ab78896df4860841bc48f166025d87ca30
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-16 16:37:12 -0700

    wip

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -18,7 +18,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
-        static::startChromeDriver();
+        //static::startChromeDriver();
     }
 
     /**
